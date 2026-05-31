7d6566c799018182a8aa3182b6085f0fc2d6dd3e
Thao-22112005 <nguyendanhthao22112005@gmail.com>
2026-05-31 23:48:02 +0700

    Lv1 Build Map

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,5 +13,9 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void LoadLv1Screen()
+    {
+        SceneManager.LoadScene("Lv1Screen");
     }
 }
